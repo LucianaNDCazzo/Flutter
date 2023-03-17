1b0e493b2b84e4ee820573e4ed343d1b4f50acbf
Luciana Cazzo <luciana.doratiotto@gmail.com>
2023-03-17 17:50:03 -0300

    170e171

--- expenses_V6/lib/components/transaction_form.dart
+++ expenses_V6/lib/components/transaction_form.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'adaptative_button.dart';
 import 'adaptative_text_field.dart';
@@ -6,16 +7,53 @@ import 'adaptative_date_picker.dart';
 class TransactionForm extends StatefulWidget {
   final void Function(String, double, DateTime) onSubmit;
 
-  const TransactionForm(this.onSubmit, {super.key});
+  TransactionForm(this.onSubmit, {super.key}) {
+    if (kDebugMode) {
+      print('Constructor TransactionForm');
+    }
+  }
 
   @override
-  State<TransactionForm> createState() => _TransactionFormState();
+  // ignore: no_logic_in_create_state
+  State<TransactionForm> createState() {
+    if (kDebugMode) {
+      print('create TransactionForm');
+    }
+    return _TransactionFormState();
+  }
 }
 
 class _TransactionFormState extends State<TransactionForm> {
   final _titleController = TextEditingController();
   final _valueController = TextEditingController();
   DateTime? _selectedDate = DateTime.now();
+
+  _TransactionFormState() {
+    if (kDebugMode) {
+      print('Constructor _TransactionFormState');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (kDebugMode) {
+      print('initState _TransactionFormState');
+    }
+  }
+
+  //@override
+  //void didUpdateWidget(Widget oldWidget) {
+  //  super.didUpdateWidget(oldWidget);
+  //  print('didUpdateWidget _TransactionFormState');
+  //}
+  @override
+  void dispose() {
+    super.dispose();
+    if (kDebugMode) {
+      print('dispose _TransactionFormState');
+    }
+  }
 
   _submitForm() {
     final title = _titleController.text;
--- expenses_V6/lib/components/transaction_list.dart
+++ expenses_V6/lib/components/transaction_list.dart
@@ -1,6 +1,6 @@
+import 'package:expenses/components/transaction_item.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -36,52 +36,7 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 5,
-                margin: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.deepPurple,
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text(
-                            'R\$${tr.value}',
-                            style: const TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    subtitle: Text(
-                      DateFormat('d MMM y').format(tr.date),
-                    ),
-                    trailing: MediaQuery.of(context).size.width > 480
-                        ? TextButton.icon(
-                            onPressed: () => onRemove(tr.id),
-                            icon: const Icon(Icons.delete, color: Colors.red),
-                            label: const Text(
-                              'Excluir',
-                              style: TextStyle(
-                                color: Colors.red,
-                              ),
-                            ),
-                          )
-                        : IconButton(
-                            icon: const Icon(Icons.delete),
-                            color: Colors.red,
-                            onPressed: () => onRemove(tr.id),
-                          )),
-              );
+              return TransactionItem(tr: tr, onRemove: onRemove);
             },
           );
   }
