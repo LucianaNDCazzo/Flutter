45dc82912ad0e968670e02d8d6e530f3edd44164
Luciana Cazzo <luciana.doratiotto@gmail.com>
2023-03-20 11:28:45 -0300

    MealsApp
    181

---
+++ meals/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vamos cozinhar?',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
