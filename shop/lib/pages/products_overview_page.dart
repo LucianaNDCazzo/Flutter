9f09a7e9e25c388a00da5e1274e6e9c47f276847
Luciana Cazzo <luciana.doratiotto@gmail.com>
2023-03-23 12:01:21 -0300

    feat: favorite-filter-example
    example using provider with stateless widget

--- shop/lib/pages/products_overview_page.dart
+++ shop/lib/pages/products_overview_page.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_grid.dart';
+import 'package:shop/models/product_list.dart';
+
+enum FilterOptions {
+  favorite,
+  all,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha loja'),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.favorite,
+                child: Text('Favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.all,
+                child: Text('Todos'),
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          ),
+        ],
       ),
       body: const Padding(
         padding: EdgeInsets.all(10),
