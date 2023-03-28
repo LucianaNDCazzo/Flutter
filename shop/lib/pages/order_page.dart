f2eb593c6c596ef47f53492301ff62e813f3cf6e
Luciana Cazzo <luciana.doratiotto@gmail.com>
2023-03-28 14:32:39 -0300

    future builder

--- shop/lib/pages/order_page.dart
+++ shop/lib/pages/order_page.dart
@@ -4,41 +4,34 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
   @override
-  State<OrdersPage> createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  Future<void> _refreshOrders(BuildContext context) {
-    return Provider.of<OrderList>(context, listen: false).loadOrders();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<OrderList>(context, listen: false).loadOrders();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: RefreshIndicator(
-        onRefresh: () => _refreshOrders(context),
-        child: Center(
-          child: ListView.builder(
-            itemCount: orders.itemsCount,
-            itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
-          ),
-        ),
-      ),
+      body: FutureBuilder(
+          future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+          builder: ((context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.error != null) {
+              return const Center(
+                child: Text('Ocorreu um erro'),
+              );
+            } else {
+              return Consumer<OrderList>(
+                builder: (ctx, orders, child) => ListView.builder(
+                    itemCount: orders.itemsCount,
+                    itemBuilder: (ctx, i) =>
+                        OrderWidget(order: orders.items[i])),
+              );
+            }
+          })),
     );
   }
 }
