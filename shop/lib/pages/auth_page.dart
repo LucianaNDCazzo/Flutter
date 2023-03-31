211701eeddc08d400992915dcb3526546e182624
Luciana Cazzo <luciana.doratiotto@gmail.com>
2023-03-31 16:19:39 -0300

    feat: more animations

--- shop/lib/pages/auth_page.dart
+++ shop/lib/pages/auth_page.dart
@@ -15,7 +15,7 @@ class AuthPage extends StatelessWidget {
             decoration: const BoxDecoration(
               gradient: LinearGradient(
                 colors: [
-                  Colors.deepPurple,
+                  Color.fromARGB(255, 142, 36, 228),
                   Color.fromRGBO(8, 240, 170, 0.898),
                 ],
                 begin: Alignment.topLeft,
@@ -23,43 +23,53 @@ class AuthPage extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(
-                    bottom: 20,
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 70,
-                  ),
-                  //cascade operator..
-                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.deepPurple,
-                      boxShadow: const [
-                        BoxShadow(
-                          blurRadius: 8,
-                          color: Colors.black54,
-                          offset: Offset(0, 2),
+          Center(
+            child: SingleChildScrollView(
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(
+                        bottom: 20,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 70,
+                      ),
+                      //cascade operator..
+                      transform: Matrix4.rotationZ(-8 * pi / 180)
+                        ..translate(-10.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(colors: [
+                          Color.fromARGB(255, 142, 36, 228),
+                          Color.fromRGBO(8, 240, 170, 0.898),
+                        ]),
+                        //color: const Color.fromRGBO(8, 240, 170, 0.898),
+                        //boxShadow: const [
+                        //  BoxShadow(
+                        //    blurRadius: 8,
+                        //    color: Colors.black54,
+                        //    offset: Offset(0, 2),
+                        //  ),
+                        //],
+                      ),
+                      child: const Text(
+                        'Minha loja',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontFamily: 'Anton',
+                          color: Colors.white,
                         ),
-                      ]),
-                  child: const Text(
-                    'Minha loja',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontFamily: 'Anton',
-                      color: Colors.white,
+                      ),
                     ),
-                  ),
+                    const AuthForm(),
+                  ],
                 ),
-                const AuthForm(),
-              ],
+              ),
             ),
           ),
         ],
